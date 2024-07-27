84a695d523b1c5b13654babca4f9d721fd21d00e
NandishChampaneria <nandishladdu7@gmail.com>
2024-07-27 19:26:51 +0530

    1- auth controllers added

---
+++ backend/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        fullName: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: 6
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        createdEvents: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Event",
+                default: []
+            }
+        ],
+        profileImg: {
+            type: String,
+            default: ""
+        },
+        bio: {
+            type: String,
+            default: ""
+        },
+        link: {
+            type: String,
+            default: ""
+        }
+    }, 
+    {timestamps: true}
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
